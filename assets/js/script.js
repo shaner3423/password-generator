e2a3d34b9d23da2feb6714e7f170c860acc5effc
Sugar Shane <scrisostomo17@gmail.com>
2021-06-11 00:45:05 -0400

    added if functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,21 @@ passwordResult = document.getElementById('password');
 
 
 
+
+if (window.confirm("Do you want numbers?")) {
+  passworldResult = randomFunction
+}
+
+if (window.confirm("Do you want lowercase letters?")) {
+  passworldResult = randomFunction
+}
+
+if (window.confirm("Do you want uppercase letters?")) {
+  passworldResult = randomFunction
+}
+
+
+
 randomFunction = {
   lower: getLower,
   upper: getUpper,
