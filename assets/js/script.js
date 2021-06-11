4322829e385b5be47b11e1eaf5b270ed5c37bfbc
Sugar Shane <scrisostomo17@gmail.com>
2021-06-11 00:06:15 -0400

    set up functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function getLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+console.log(getLower());
+
+function getUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+console.log(getUpper());
+
+function getNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log(getNumber());
+
+function getSymbol() {
+  symbols = '!@#$%^&*(){}[]=></'
+  return symbols [Math.floor(Math.random() * symbols.length)];
+}
+console.log(getSymbol());
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
